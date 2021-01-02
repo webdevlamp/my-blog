960746a69a4ed8912d5a2d4a362b6d9b83185b0d
Mayur Bhatt <mayur.bhatt@gmail.com>
2021-01-02 14:51:38 +0530

    adding posts to the homepage

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $posts = \App\Models\Post::all();
+
+    return view('welcome', ['posts' => $posts]);
 });
